c7a6c27a38058e9c8a5a31c1747c8160e4094ecd
Switchback <aidartemirgaliev@gmail.com>
2022-11-15 22:52:53 +0600

    add functional to find, upload flights

--- src/main/java/org/example/airline/GreetingController.java
+++ src/main/java/org/example/airline/GreetingController.java
@@ -1,14 +1,29 @@
 package org.example.airline;
 
+import org.example.airline.domain.Flight;
+import org.example.airline.repos.FlightRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private FlightRepo flightRepo;
+
     @GetMapping("/greeting")
     public String greeting(
             @RequestParam(name="", required = false, defaultValue = "world") String name, Map<String, Object> model
@@ -19,7 +34,49 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("something", "Welcome to the Flight booking website!");
+        Iterable<Flight> flights = flightRepo.findAll();
+
+
+        model.put("flights", flights);
+
+        return "main";
+    }
+
+    @PostMapping
+    public String add( @RequestParam String fromCity, @RequestParam String toCity,
+                       @RequestParam String airplane, @RequestParam float price,
+                       @RequestParam String departureDate, @RequestParam String departureTime,
+                       Map<String, Object> model) {
+
+
+        Flight flight = new Flight( fromCity, toCity, airplane, price, departureDate, departureTime );
+
+        flightRepo.save(flight);
+
+        Iterable<Flight> flights = flightRepo.findAll();
+
+        model.put("flights", flights);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String toCity, @RequestParam String fromCity,
+                         @RequestParam String searchFlightDate, Map<String, Object> model) {
+
+        SimpleDateFormat fromUser = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat myFormat = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            searchFlightDate = myFormat.format( fromUser.parse(searchFlightDate) );
+        } catch( ParseException e ) {
+            e.printStackTrace();
+        }
+
+        List<Flight> targetCities =
+                flightRepo.findByFromCityAndToCityAndDepartureDate( fromCity, toCity, searchFlightDate );
+
+        model.put("flights", targetCities);
+
         return "main";
     }
 }
