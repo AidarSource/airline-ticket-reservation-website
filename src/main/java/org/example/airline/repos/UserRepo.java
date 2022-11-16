7e60cf2e42a1e19e0b281c8d15a51fec8a008466
Switchback <aidartemirgaliev@gmail.com>
2022-11-17 00:26:30 +0600

    add user repo for CRUD

---
+++ src/main/java/org/example/airline/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package org.example.airline.repos;
+
+import org.example.airline.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
