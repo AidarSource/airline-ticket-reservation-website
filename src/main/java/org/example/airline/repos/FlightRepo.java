b4b8a18e840a223169351aaf29c6365237b54709
Switchback <aidartemirgaliev@gmail.com>
2022-11-23 23:00:14 +0600

    add findById method

--- src/main/java/org/example/airline/repos/FlightRepo.java
+++ src/main/java/org/example/airline/repos/FlightRepo.java
@@ -6,5 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface FlightRepo extends CrudRepository<Flight, Long> {
+    Flight findById(Integer id);
     List<Flight> findByFromCityAndToCityAndDepartureDate(String fromCity, String toCity, String departureDate);
 }
