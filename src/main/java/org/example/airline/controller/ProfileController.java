6bf80d5070ef05862a750bead159cb4bc3a59fa7
Switchback <aidartemirgaliev@gmail.com>
2022-11-23 23:07:53 +0600

    add user profile controller

---
+++ src/main/java/org/example/airline/controller/ProfileController.java
@@ -0,0 +1,30 @@
+package org.example.airline.controller;
+
+import org.example.airline.domain.User;
+import org.example.airline.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ProfileController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    User user;
+
+    @GetMapping("/profile/{username}")
+    public String profile( @PathVariable String username, Model model) {
+
+        User user = userRepo.findByUsername( username );
+
+        model.addAttribute( "tickets", user.getTickets() );
+
+        return "profile";
+    }
+}
