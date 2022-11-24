bc980497ae095fc0087c7b9b3b355b8149f23824
Switchback <aidartemirgaliev@gmail.com>
2022-11-24 22:04:52 +0600

    add refund post mapping for ticket

--- src/main/java/org/example/airline/controller/ProfileController.java
+++ src/main/java/org/example/airline/controller/ProfileController.java
@@ -1,5 +1,6 @@
 package org.example.airline.controller;
 
+import org.example.airline.domain.Flight;
 import org.example.airline.domain.User;
 import org.example.airline.repos.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,5 +27,16 @@ public class ProfileController {
         model.addAttribute( "tickets", user.getTickets() );
 
         return "profile";
+    }
+
+    @PostMapping("/refund")
+    public String refund(@RequestParam String username, @RequestParam int ticketId) {
+        User user = userRepo.findByUsername( username );
+
+        user.removeTicket( ticketId );
+
+        userRepo.save( user );
+
+        return "redirect:/profile/" + username;
     }
 }
