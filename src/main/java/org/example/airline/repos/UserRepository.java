c6731759c039b87ea20348d9de3d1e64dbf6d0a5
Switchback <aidartemirgaliev@gmail.com>
2022-12-03 15:02:31 +0600

    Renamed to UserRepository

--- src/main/java/org/example/airline/repos/UserRepository.java
+++ src/main/java/org/example/airline/repos/UserRepository.java
@@ -1,8 +1,8 @@
 package org.example.airline.repos;
 
-import org.example.airline.domain.User;
+import org.example.airline.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepo extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
